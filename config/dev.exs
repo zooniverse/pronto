58bad61c94492198b685bf5fc11419d3c1d336c3
Marten Veldthuis <marten@veldthuis.com>
2016-07-05 17:12:41 +0100

    Dockerize

--- config/dev.exs
+++ config/dev.exs
@@ -24,8 +24,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :pronto, Pronto.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: System.get_env("USER") || "postgres",
-  password: nil,
-  database: "pronto_dev",
-  hostname: "localhost",
+  username: System.get_env("POSTGRES_USER") || System.get_env("USER") || "postgres",
+  password: System.get_env("POSTGRES_PASS") || nil,
+  database: System.get_env("POSTGRES_DB")   || "pronto_dev",
+  hostname: System.get_env("POSTGRES_HOST") || "localhost",
   pool_size: 10
